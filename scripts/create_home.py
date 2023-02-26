31b36e06dd1434278c2d784628a785c5d48a03b0
agustin <agustin.piqueres@gmail.com>
2023-02-26 21:11:01 +0100

    feat: add generator for spanish version

--- scripts/create_home.py
+++ scripts/create_home.py
@@ -55,7 +55,7 @@ def create_list(data: list[list[str] | str]) -> str:
         return md.list([md.link(item, item) for item in data])
 
 
-def content(blogs: list[str], podcasts: list[list[str]], books: list[list[str]]) -> str:
+def content(blogs: list[str], podcasts: list[list[str]], books: list[list[str]], lang: str = "en") -> str:
     front_matter = textwrap.dedent(
         """---
 title: Home
@@ -68,40 +68,56 @@ weight: 3
     md = MarkdownRenderer()
     md.add(front_matter)
 
-    md.add(
-        """The following lists of items correspond to content that has
-helped me to learn (and still does), and would like to keem them
+    if lang == "en":
+        text = """The following lists of items correspond to content that has
+helped me to learn (and still does), and would like to keep them
 located either for future use or just in case I want to share
 with anybody. None of it is in any specific order.
 """
-    )
+    else:
+        text = """Las siguientes listas corresponden a contenido que me ha 
+ayudado a aprender (y sigue haciéndolo), y me gustaría mantener lcalizadas
+tanto para uso futuro como por si quiero compartirlo con cualquier persona.
+No siguen ningún orden específico.
+        """
+
+    md.add(text)
+    if lang == "en":
+        title_blogs = "📔 Blogs I tend to visit from time to time"
+        title_books = "📻 Podcasts I listen"
+        title_podcasts = "📚 Books I have read (completely or some parts), or that are waiting to be read"
+    else:
+        title_blogs = "📔 Blogs que suelo visitar de vez en cuando"
+        title_books = "📻 Podcasts que escucho"
+        title_podcasts = "📚 Books que he leído (completos o alguna parte), o que esperan a ser leídos"
 
     md.add(
         do_clickable(
-            create_list(blogs), title="📔 Blogs I tend to visit from time to time"
+            create_list(blogs), title=title_blogs
         )
     )
-    md.add(do_clickable(create_list(podcasts), title="📻 Podcasts I listen"))
+    md.add(do_clickable(create_list(podcasts), title=title_books))
     md.add(
         do_clickable(
             create_list(books),
-            title="📚 Books I have read (completely or some parts), or that are waiting to be read",
+            title=title_podcasts,
         )
     )
 
     return md.text
 
 
-def create_home_file(filename: Path) -> None:
+def create_home_file(filename: Path, lang: str = "en") -> None:
     """Creates the home file."""
     blogs = read_csv(BLOGS)
     podcasts = read_csv(PODCASTS, num_columns=2)
     books = read_csv(BOOKS, num_columns=2)
 
-    data = content(blogs, podcasts, books)
+    data = content(blogs, podcasts, books, lang=lang)
 
     filename.write_text(data, encoding="utf-8")
 
 
 if __name__ == "__main__":
-    create_home_file(ROOT / "content" / "home" / "_index.md")
+    create_home_file(ROOT / "content" / "home" / "_index.en.md")
+    create_home_file(ROOT / "content" / "home" / "_index.es.md", lang="es")
