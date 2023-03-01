d7d13ab21ebad92027a96579e8f85dcf45242a1b
agustin <agustin.piqueres@gmail.com>
2023-03-01 20:50:05 +0100

    feat: home version in english and spanish

--- scripts/create_home.py
+++ scripts/create_home.py
@@ -55,7 +55,158 @@ def create_list(data: list[list[str] | str]) -> str:
         return md.list([md.link(item, item) for item in data])
 
 
-def content(blogs: list[str], podcasts: list[list[str]], books: list[list[str]], lang: str = "en") -> str:
+def add_projects(lang: str = "en") -> str:
+    md_proj = MarkdownRenderer()
+    if lang == "en":
+        md_proj.add(md.title(2, "Projects"))
+
+        translate_md = md.code("translate_md")
+        translate_md += """: {}. A client to translate markdwon files from english to spanish,
+makes use of {} for the translation service.""".format(
+            md.bold("Work in progress"),
+            md.link("spanglish", "https://github.com/plaguss/spanglish"),
+        )
+
+        helpner_core = md.link(
+            md.code("helpner-core"), "https://github.com/plaguss/helpner-core"
+        )
+        cli_help_maker = md.link(
+            md.code("cli-help-maker"), "https://github.com/plaguss/cli-help-maker"
+        )
+        helpner = md.link(md.code("helpner"), "https://github.com/plaguss/helpner")
+        helpner += """: A python console program to extract entities from command line 
+programs' help messages. It makes use of two intermediate libraries: {} as a
+spaCy template, and {} as a data generator.""".format(
+            helpner_core, cli_help_maker
+        )
+
+        movinets_for_crossfit = md.code("MoViNets for CrossFit")
+        movinets_for_crossfit += """: A plotly dash app to classify CrossFit movements {} 
+deployed to AWS, based on a finetuned version of {}, the tensorflow implementation of 
+a network to classify human movements in videoclips. The repos still need to be better
+organized, but los entre los comopnentes, a helper library can be seen here: {}, the 
+code for the front here: {}, and here the pieces related to the cloud: {}.
+""".format(
+            md.italic("(no longer)"),
+            md.link(
+                "Mobile Video Networks (MoViNets)",
+                "https://github.com/tensorflow/models/tree/master/official/projects/movinet",
+            ),
+            md.link("movinets_helper", "https://github.com/plaguss/movinets_helper"),
+            md.link(
+                "movinets_dash_app", "https://github.com/plaguss/movinets_dash_app"
+            ),
+            md.link(
+                "lambda_aws", "https://github.com/plaguss/tfm-misc/tree/main/lambda_aws"
+            ),
+        )
+
+        pytokei = md.link(md.code("pytokei"), "https://github.com/plaguss/pytokei")
+        pytokei += f""": Python bindings to {md.link("tokei", "https://github.com/XAMPPRocky/tokei")}, 
+a rust program to extract statistics from your code."""
+
+        biblioteca_cidaen = md.link(
+            md.code("biblioteca-cidaen"), "https://github.com/plaguss/biblioteca-cidaen"
+        )
+        biblioteca_cidaen += """: {}. A prototype for an 'online library' where the students could
+share their master's thesis.
+Even though its tailored for the master's program that I coursed, 
+it can be used as an interesting approach to introduce students
+to collaboration in GitHub and the Pull Request mecanism.""".format(
+            md.italic("(written in spanish)")
+        )
+        md_proj.add(
+            md.list(
+                [
+                    translate_md,
+                    helpner,
+                    movinets_for_crossfit,
+                    pytokei,
+                    biblioteca_cidaen,
+                ]
+            )
+        )
+
+    else:
+        md_proj.add(md.title(2, "Proyectos"))
+
+        translate_md = md.code("translate_md")
+        translate_md += """: {}. Un cliente para traducir ficheros markdown de inglés a 
+castellano, hace uso de {} para el servicio de traducción.""".format(
+            md.bold("Work in progress"),
+            md.link("spanglish", "https://github.com/plaguss/spanglish"),
+        )
+
+        helpner_core = md.link(
+            md.code("helpner-core"), "https://github.com/plaguss/helpner-core"
+        )
+        cli_help_maker = md.link(
+            md.code("cli-help-maker"), "https://github.com/plaguss/cli-help-maker"
+        )
+        helpner = md.link(md.code("helpner"), "https://github.com/plaguss/helpner")
+        helpner += """: Un programa por consola escrito en python para extraer entidades
+de mensajes de ayuda de programas por consola. Hace uso de dos librerías intermedias: {}
+una plantilla de spaCy, y {} como generador de datos""".format(
+            helpner_core, cli_help_maker
+        )
+
+        movinets_for_crossfit = md.code("MoViNets para CrossFit")
+        movinets_for_crossfit += """: Una aplicación escrita en {} para clasificar movimientos
+de CrossFit {} desplegada en AWS, basada en una versión 'fine tuned' de {}, la implementación
+en tensorflow de una red para clasificar movimientos humanos en videoclips. Los repositorios
+aún necesitan ponerse al día, pero entre los componentes, una librería de ayuda se puede ver
+aquí: {}, el código para el front aquí: {}, y las piezas relacionadas con la nube aquí: {}""".format(
+            md.link("plotly dash", "https://dash.plotly.com/"),
+            md.italic("(no actualmente)"),
+            md.link(
+                "Mobile Video Networks (MoViNets)",
+                "https://github.com/tensorflow/models/tree/master/official/projects/movinet",
+            ),
+            md.link("movinets_helper", "https://github.com/plaguss/movinets_helper"),
+            md.link(
+                "movinets_dash_app", "https://github.com/plaguss/movinets_dash_app"
+            ),
+            md.link(
+                "lambda_aws", "https://github.com/plaguss/tfm-misc/tree/main/lambda_aws"
+            ),
+        )
+
+        pytokei = md.link(md.code("pytokei"), "https://github.com/plaguss/pytokei")
+        pytokei += f""": Bindings en python para {md.link("tokei", "https://github.com/XAMPPRocky/tokei")}, 
+un programa escrito en rust para extraer estadísticos sobre el código fuente."""
+
+        biblioteca_cidaen = md.link(
+            md.code("biblioteca-cidaen"), "https://github.com/plaguss/biblioteca-cidaen"
+        )
+        biblioteca_cidaen += """: {}. Prototipo de una 'librería online' donde los estudiantes
+podrían compartir sus trabajos (de máster en este caso).
+Aunque está hecho a la medida del programa de máster que cursé, se puede
+utilizar como un enfoque interesante para ayudar introducir a los alumnos
+a la colaboración en GitHub mediante Pull Requests.""".format(
+            md.italic("(written in spanish)")
+        )
+        md_proj.add(
+            md.list(
+                [
+                    translate_md,
+                    helpner,
+                    movinets_for_crossfit,
+                    pytokei,
+                    biblioteca_cidaen,
+                ]
+            )
+        )
+
+
+    return md_proj.text
+
+
+def content(
+    blogs: list[str],
+    podcasts: list[list[str]],
+    books: list[list[str]],
+    lang: str = "en",
+) -> str:
     front_matter = textwrap.dedent(
         """---
 title: Home
@@ -68,18 +219,23 @@ weight: 3
     md = MarkdownRenderer()
     md.add(front_matter)
 
+    projects = add_projects(lang=lang)
+    md.add(projects)
+
     if lang == "en":
-        text = """The following lists of items correspond to content that has
+        text = md.title(2, "Some wonderful resources")
+        text += """\nThe following lists of items correspond to content that has
 helped me to learn (and still does), and would like to keep them
 located either for future use or just in case I want to share
 with anybody. None of it is in any specific order.
 """
     else:
-        text = """Las siguientes listas corresponden a contenido que me ha 
+        text = md.title(2, "Algunos recursos maravillosos")
+        text += """\nLas siguientes listas corresponden a contenido que me ha 
 ayudado a aprender (y sigue haciéndolo), y me gustaría mantener lcalizadas
 tanto para uso futuro como por si quiero compartirlo con cualquier persona.
 No siguen ningún orden específico.
-        """
+"""
 
     md.add(text)
     if lang == "en":
@@ -91,11 +247,7 @@ No siguen ningún orden específico.
         title_books = "📻 Podcasts que escucho"
         title_podcasts = "📚 Books que he leído (completos o alguna parte), o que esperan a ser leídos"
 
-    md.add(
-        do_clickable(
-            create_list(blogs), title=title_blogs
-        )
-    )
+    md.add(do_clickable(create_list(blogs), title=title_blogs))
     md.add(do_clickable(create_list(podcasts), title=title_books))
     md.add(
         do_clickable(
